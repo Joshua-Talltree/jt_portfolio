1e45250f1add8b7b3902d463d4c27312cd95ded5
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-07-01 23:34:00 -0500

    feat: working on posts page, still need to get the post to render.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import {Link} from "react-router-dom";
 import sanityClient from "../client.js";
 
 export default function Post() {
@@ -26,17 +27,22 @@ export default function Post() {
             <section className="container mx-auto">
                 <h1 className="text-5xl flex justify-center">Blog Posts Page</h1>
                 <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page of blog posts</h2>
-                <div>
-                    <article>
-                        {/*<Link>*/}
-                        <span>
-                            <img />
-                            <span>
-                                <h3></h3>
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {postData && postData.map((post, index) => (
+                        <article>
+                            <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                        <span className="block h-64 relative rounded shadow leading-snug bg-white border-green-400"
+                              key={index}>
+                            <img src={post.mainImage.asset.url} alt={post.mainImage.alt}
+                                 className="w-full h-full rounded-r object-cover absolute"/>
+                            <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
+                                <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-blue-700 bg-opacity-75 rounded" />
+                                {post.title}
                             </span>
                         </span>
-                        {/*</Link>*/}
-                    </article>
+                            </Link>
+                        </article>
+                    ))}
                 </div>
             </section>
         </main>
